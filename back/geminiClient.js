9d1846a3f2184df5a534d21466451ac05d06e180
Ratouit Lucas <ratouitlucaspro@gmail.com>
2026-01-22 11:47:03 +0100

    minimal back setup

---
+++ back/geminiClient.js
@@ -0,0 +1,16 @@
+import { GoogleGenAI } from "@google/genai";
+
+const genAI = new GoogleGenAI({
+    apiKey: process.env.GEMINI_API_KEY,
+});
+
+// 3. Fonction simple d’appel
+export async function generateText(prompt) {
+  console.log("Generating text with prompt:", prompt);
+  const res = await genAI.models.generateContent({
+    model: "gemini-3-flash-preview",
+    contents: prompt,
+  });
+
+  return res.candidates[0].content.parts[0].text;
+}
---
+++ back/index.js
@@ -0,0 +1,49 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import { generateText } from "./geminiClient.js";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+const messageList = [];
+
+const promptContextTest = `
+Tu es un narrateur de RPG.
+Continue cette histoire en 3 phrases,
+en prenant en compte les précédents messages [${messageList.join(", ")}]
+(En sachant que les messages impairés sont des actions du joueur
+et les messages pairs sont des actions de l'IA). :
+`;
+
+app.get("/ai/messages", (req, res) => {
+  res.json({ messages: messageList });
+});
+
+app.post("/ai/generate", async (req, res) => {
+  const userMessage = req.body.prompt;
+  if (!userMessage) {
+    return
+  }
+  const prompt = `${promptContextTest} ${userMessage}`;
+  if (!prompt) {
+    return res.status(400).send("Prompt query parameter is required.");
+  }
+
+  try {
+    const text = await generateText(prompt);
+    messageList.push(userMessage);
+    messageList.push(text);
+    res.json({ text });
+  } catch (error) {
+    console.error("Error generating text:", error);
+    res.status(500).send("Error generating text.");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
