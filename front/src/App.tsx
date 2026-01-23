5304c7c496387ec16cd06b9a4d751d57e37fcc69
Ratouit Lucas <ratouitlucaspro@gmail.com>
2026-01-23 02:20:01 +0100

    corriger l'URL de l'API dans la configuration de l'application

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   const [prompt, setPrompt] = useState("");
   const [messageList, setMessageList] = useState<string[]>([]);
 
-  const API_URL = import.meta.env.VITE_API_URL || "http://back:3000/api";
+  const API_URL = import.meta.env.VITE_API_URL || "/api";
 
   const getMessages = async () => {
     await axios.get(`${API_URL}/ai/messages`).then((res) => {
